894c920ee84c93faa151755d7c95a79ffcbad3c1
Francisco das Chagas Silva Junior <francisco.junior@hbsis.com.br>
2020-09-07 22:57:47 -0300

    Finalizada a API

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    //https://localhost:5001/products
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        [AllowAnonymous]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking().ToListAsync();
+
+            return Ok(products);
+        }
+
+
+        [HttpGet]
+        [Route("{id:int}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<Product>> GetById(
+           int id,
+           [FromServices] DataContext context)
+        {
+            var product = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+            if (product == null)
+                return NotFound(new { message = "Produto não encontrado" });
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<List<Product>>> GetByCategory(
+            int id,
+            [FromServices] DataContext context)
+        {
+            var products = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .Where(x => x.CategoryId == id)
+                .ToListAsync();
+
+            return Ok(products);
+        }
+
+
+        [HttpPost]
+        [Route("")]
+        [Authorize(Roles = "employee")]
+        public async Task<ActionResult<List<Product>>> Post(
+            [FromBody] Product model,
+            [FromServices] DataContext context)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar o produto" });
+            }
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using Shop.Services;
+
+namespace Shop.Controllers
+{
+    //https://localhost:5001/users
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        [Authorize(Roles = "manager")]
+        public async Task<ActionResult<List<User>>> Get([FromServices] DataContext context)
+        {
+            var users = await context
+                .Users
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpPost]
+        [Route("")]
+        [AllowAnonymous]
+        public async Task<ActionResult<User>> Post(
+                    [FromServices] DataContext context,
+                    [FromBody] User model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                //Forçar o usuário a sempre ser Funcionário
+                model.Role = "employee";
+
+                context.Users.Add(model);
+                await context.SaveChangesAsync();
+
+                //Esconde a senha
+                model.Password = "";
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar o usuário" });
+            }
+        }
+
+
+        [HttpPut]
+        [Route("{id:int}")]
+        [Authorize(Roles = "manager")]
+        public async Task<ActionResult<User>> Put(
+            [FromServices] DataContext context,
+            int id,
+            [FromBody] User model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (id != model.Id)
+                return NotFound(new { message = "Usuário não encontrado" });
+
+            try
+            {
+                context.Entry(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível alterar o usuário" });
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate(
+                    [FromServices] DataContext context,
+                    [FromBody] User model)
+        {
+            var user = await context
+                .Users
+                .AsNoTracking()
+                .Where(x => x.UserName == model.UserName && x.Password == model.Password)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
